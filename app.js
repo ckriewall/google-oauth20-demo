4b63a285f4bc0a086d3909b9558b39276f698f82
Charlie Kriewall <charlie@kriewall.com>
2020-12-15 17:29:39 -0800

    move userModel to separate file

--- app.js
+++ app.js
@@ -1,27 +1,21 @@
 require('dotenv').config()
 const express = require('express')
-const bodyParser = require('body-parser')
 const ejs = require('ejs')
 const mongoose = require('mongoose')
 const session = require('express-session')
 const passport = require('passport')
-const passportLocalMongoose = require('passport-local-mongoose')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
-const findOrCreate = require('mongoose-findorcreate')
 
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
+const User = require('./userModel.js')
 const app = express()
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
 
 app.use(
   session({
-    secret: 'Our little secret.',
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
   })
@@ -35,18 +29,6 @@ mongoose.connect('mongodb://localhost:27017/userDB', {
   useUnifiedTopology: true,
   useCreateIndex: true,
 })
-
-const userSchema = new mongoose.Schema({
-  email: String,
-  password: String,
-  googleId: String,
-  nickname: String,
-})
-
-userSchema.plugin(passportLocalMongoose)
-userSchema.plugin(findOrCreate)
-
-const User = new mongoose.model('User', userSchema)
 
 passport.use(User.createStrategy())
 
@@ -107,7 +89,6 @@ app.get('/public-info', function (req, res) {
 
 app.get('/private-info', function (req, res) {
   if (req.isAuthenticated()) {
-    console.log(passport)
     res.render('private-info', {
       fname: 'Charlie',
       email: 'charlie@kriewall.com',
@@ -115,26 +96,6 @@ app.get('/private-info', function (req, res) {
   } else {
     res.redirect('/')
   }
-})
-
-app.post('/private-info', function (req, res) {
-  const submittedSecret = req.body.secret
-
-  //Once the user is authenticated and their session gets saved, their user details are saved to req.user.
-  // console.log(req.user.id);
-
-  User.findById(req.user.id, function (err, foundUser) {
-    if (err) {
-      console.log(err)
-    } else {
-      if (foundUser) {
-        foundUser.secret = submittedSecret
-        foundUser.save(function () {
-          res.redirect('/public-info')
-        })
-      }
-    }
-  })
 })
 
 app.get('/logout', function (req, res) {
